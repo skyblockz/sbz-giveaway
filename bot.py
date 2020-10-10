f78ea9ddf08bd6fccf5af084f5e0b4e45a7673bc
skyblockz <skyblockzpublic@gmail.com>
2020-10-10 22:11:41 +0800

    check_giveaways task added

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
+import datetime
 import json
 import logging
+import traceback
 
 import asyncpg
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 import db
 
@@ -39,4 +41,73 @@ async def on_ready():
     print('Ready')
 
 
+@tasks.loop(seconds=1)
+async def check_giveaways():
+    if not bot.loaded_db:
+        # bot hasnt loaded DB yet
+        return
+    need_rolling = await db.get_need_rolling_giveaways(bot.db)
+    if len(need_rolling) == 0:
+        pass
+    else:
+        for ga_id in need_rolling:
+            try:
+                winners = await db.roll_winner(bot.db, ga_id)
+            except db.NoParticipants:
+                details = await db.get_info_of_giveaway(bot.db, ga_id)
+                embed = discord.Embed(title=details['prize_name'],
+                                      description='No one has joined the giveaway, thus the roll has been canceled',
+                                      colour=discord.Colour.dark_red())
+                embed.add_field(name='Hosted By', value=details['host'])
+                if details['image'] is not None:
+                    embed.set_image(url=details['image'])
+                embed.timestamp = datetime.datetime.utcfromtimestamp(details['created_at'] + details['length'])
+                embed.set_footer(text=f'ID: {ga_id}| Ended At')
+                cc = bot.get_channel(details['channel_id'])
+                mc = await cc.fetch_message(details['message_id'])
+                await mc.edit(embed=embed)
+                await cc.send(
+                    f'Giveaway of {details["prize_name"]} (ID:{str(ga_id)}) has been canceled, due to no participants in the giveaway')
+                continue
+            except db.NotEnoughParticipants:
+                details = await db.get_info_of_giveaway(bot.db, ga_id)
+                embed = discord.Embed(title=details['prize_name'],
+                                      description=f'Not enough people joined the giveaway (only {len(details["participants"])}), thus the roll has been canceled',
+                                      colour=discord.Colour.dark_red())
+                embed.add_field(name='Hosted By', value=details['host'])
+                if details['image'] is not None:
+                    embed.set_image(url=details['image'])
+                embed.timestamp = datetime.datetime.utcfromtimestamp(details['created_at'] + details['length'])
+                embed.set_footer(text=f'ID: {ga_id}| Ended At')
+                cc = bot.get_channel(details['channel_id'])
+                mc = await cc.fetch_message(details['message_id'])
+                await mc.edit(embed=embed)
+                await cc.send(
+                    f'Giveaway of {details["prize_name"]} (ID:{str(ga_id)}) has been canceled, due to not enough people joined the giveaway (only {len(details["participants"])})')
+                continue
+            details = await db.get_info_of_giveaway(bot.db, ga_id)
+            winners_ping = [f'<@!{i}>' for i in details['winners']]
+            embed = discord.Embed(title=details['prize_name'],
+                                  description=f'Winner{"s" if details["winner_count"] > 1 else ""}: {" ".join(winners_ping)}'
+                                  )
+            embed.add_field(name='Hosted By', value=details['host'])
+            if details['image'] is not None:
+                embed.set_image(url=details['image'])
+            embed.timestamp = datetime.datetime.utcfromtimestamp(details['created_at'] + details['length'])
+            embed.set_footer(text=f'ID: {ga_id}| Ended At')
+            cc = bot.get_channel(details['channel_id'])
+            mc = await cc.fetch_message(details['message_id'])
+            await mc.edit(embed=embed)
+            await cc.send(
+                f'Giveaway of {details["prize_name"]} (ID:{str(ga_id)}) has been rolled, winners: {" ".join(winners_ping)}')
+
+
+
+@check_giveaways.error
+async def check_giveaways_error_handler(error):
+    logging.error('Task check_giveaways has failed')
+    traceback.print_exception(type(error), error, error.__traceback__)
+
+
+check_giveaways.start()
 bot.run(tokens['token'])
