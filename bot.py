3a2797a9ef311ac8cba1a6c5c9b250d16aeccf4f
skyblockz <skyblockzpublic@gmail.com>
2020-10-10 23:24:46 +0800

    All wrapped up for today

--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
+import asyncio
 import datetime
 import json
 import logging
+import re
 import traceback
 
 import asyncpg
 import discord
+import humanize
 from discord.ext import commands, tasks
+from discord.ext.commands import TextChannelConverter, BadArgument, MemberConverter
 
 import db
 
@@ -24,6 +28,27 @@ bot.load_extension('jishaku')
 
 with open('token.json', 'r') as f:
     tokens = json.loads(f.read())
+
+
+def convert_time(raw):
+    """
+    Convert human time (eg. 10h10m) into seconds
+
+    :param raw: Human time
+    :return: Seconds
+    """
+    res = re.split(r'(\d+)', raw)
+    res.pop(0)
+    total_time = 0
+    current_working = 0
+    time_suffixes = {'w': 604800, 'd': 86400, 'h': 3600, 'm': 60, 's': 1}
+    for i in res:
+        if i.isdigit():
+            current_working = i
+        else:
+            total_time += int(current_working) * time_suffixes[i]
+            current_working = 0
+    return total_time
 
 
 @bot.event
@@ -102,11 +127,76 @@ async def check_giveaways():
                 f'Giveaway of {details["prize_name"]} (ID:{str(ga_id)}) has been rolled, winners: {" ".join(winners_ping)}')
 
 
-
 @check_giveaways.error
 async def check_giveaways_error_handler(error):
     logging.error('Task check_giveaways has failed')
     traceback.print_exception(type(error), error, error.__traceback__)
+
+
+class Canceled(Exception):
+    pass
+
+
+@bot.command(name='new', usage='new', description='Launches an interactive session of creating a new giveaway')
+async def new_giveaway(ctx):
+    def check(message):
+        if message.content == 'cancel':
+            raise Canceled
+        return message.author.id == ctx.author.id and message.channel.id == ctx.channel.id
+
+    await ctx.send(
+        'Welcome to Interactive Giveaway Creator, please answer some questions before you make a giveaway.\nType `cancel` anytime to terminate the interactive session\n\n1. Where would you like to create the giveaway at?')
+    try:
+        msg = await bot.wait_for('message', check=check, timeout=240)
+        channel_id = (await TextChannelConverter().convert(ctx, msg.content)).id
+        await ctx.send(
+            f'Channel will be <#{str(channel_id)}>\n\n2.How long should the giveaway last for? (suffixes: w-week d-day h-hour, m-minute, s-second)')
+        msg = await bot.wait_for('message', check=check, timeout=240)
+        length = convert_time(msg.content)
+        await ctx.send(
+            f'It will last for {humanize.naturaltime(length, future=True).rstrip(" from now")}\n\n3. How many winners should there be?')
+        msg = await bot.wait_for('message', check=check, timeout=240)
+        winner_count = int(msg.content)
+        await ctx.send(
+            f'There will be {str(winner_count)} winner{"s" if winner_count > 1 else ""}\n\n4. What should the prize be?')
+        msg = await bot.wait_for('message', check=check, timeout=240)
+        prize_name = msg.content
+        await ctx.send(
+            f'The prize will be {prize_name}\n\n5. Any showcase images for it? (Only one allowed) (Please post a link to the file or upload it as an attachment) (Type none or n if there isn\'t any)')
+        msg = await bot.wait_for('message', check=check, timeout=240)
+        if msg.content.lower() == 'none' or msg.content.lower() == 'n':
+            image = None
+        else:
+            if len(msg.attachments) <= 0:
+                image = msg.content
+            else:
+                image = msg.attachments[0].url
+        await ctx.send(
+            f'Image URL will be {image}\n\n6. Who donated for the giveaway? (You can enter their user ID / name#1234 or ping them)')
+        msg = await bot.wait_for('message', check=check, timeout=240)
+        host = (await MemberConverter().convert(ctx, msg.content)).id
+        await ctx.send(
+            f'<@!{host}> hosted the giveaway\n\n7. Set any requirements on this giveaway (Type in the role IDs, separate them in space, type none or n if there isn\'t any)')
+        msg = await bot.wait_for('message', check=check, timeout=240)
+        if msg.content.replace(' ', '').isdigit():
+            requirements = [int(i) for i in msg.content.split(' ')]
+        else:
+            requirements = None
+        req_ping = [f'<@&{i}>' for i in requirements]
+        await ctx.send(
+            f'Please validate your selections:\n\nCID `{channel_id}`\nLGT `{length}`\nWNC `{winner_count}`\nPZN `{prize_name}`\nIMG `{image}`\nHST `{host}`\nREQ {", ".join(req_ping)}\n\nType `yes` to start the giveaway', allowed_mentions=discord.AllowedMentions.none())
+        msg = await bot.wait_for('message', check=check, timeout=240)
+        if 'y' not in msg.content:
+            raise InterruptedError
+        await ctx.send('it should create smth now')
+    except asyncio.TimeoutError:
+        await ctx.send('Timed out, all inputs have been discarded.')
+    except Canceled:
+        await ctx.send('Canceled, all inputs have been discarded')
+    except BadArgument:
+        await ctx.send('Channel or host invalid, terminating interactive session, all inputs have been discarded')
+    except InterruptedError:
+        await ctx.send('Last validation did not pass, terminating interactive session, all inputs have been discarded')
 
 
 check_giveaways.start()
